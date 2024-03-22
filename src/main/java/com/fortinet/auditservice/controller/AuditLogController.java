4cec4fbcf086e5e144dbf78b4b6a31370707e1c3
Phani Karthik Maradani <phani.karthik@interface.ai>
2024-03-22 17:17:56 +0530

    basic auth with admin admin working

--- src/main/java/com/fortinet/auditservice/controller/AuditLogController.java
+++ src/main/java/com/fortinet/auditservice/controller/AuditLogController.java
@@ -2,7 +2,6 @@ package com.fortinet.auditservice.controller;
 
 import com.fortinet.auditservice.interfaces.AuditLogService;
 import com.fortinet.auditservice.model.AuditLog;
-import jakarta.validation.Valid;
 import lombok.NonNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,7 +12,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
